1598971eff27edb9e39d425d07e5d1a2eaf2a905
vmdamian <visanmihaidamian@gmail.com>
2020-04-25 15:43:04 +0300

    Added docker compose for elastic serach cluster and start/stop scripts for es and cassandra databases.

---
+++ healthmonitoroperational/start-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "Deleting all pre-existent docker containers."
+docker rm -f $(docker ps -a -q)
+echo -e "Starting Cassandra"
+docker run --name cassandra --rm -p 9042:9042 -d -v /var/healthmonitor/cassandra/data:/var/lib/cassandra cassandra 
+echo -e "Starting Elasticsearch"
+docker-compose -f docker-compose-elasticsearch.yml up -d
+
