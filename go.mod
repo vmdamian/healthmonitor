01888fab8df4bd39813c728fcae01fc0082b4ee9
vmdamian <visanmihaidamian@gmail.com>
2020-05-30 17:18:52 +0300

    Fixed permissions, added go modules files.

---
+++ go.mod
@@ -0,0 +1,13 @@
+module healthmonitor
+
+go 1.14
+
+require (
+	github.com/gocql/gocql v0.0.0-20200410100145-b454769479c6
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.4
+	github.com/olivere/elastic v6.2.30+incompatible
+	github.com/olivere/elastic/v7 v7.0.14
+	github.com/sirupsen/logrus v1.5.0
+)
--- healthmonitoroperational/start-setup.sh
+++ healthmonitoroperational/start-setup.sh
@@ -6,20 +6,9 @@ docker rm -f $(docker ps -a -q)
 sleep 5s
 
 echo -e "Starting Cassandra"
-docker run --name cassandra --rm -p 9042:9042 -d -v /var/healthmonitor/cassandra/data:/var/lib/cassandra cassandra 
+docker run --name cassandra --rm -p 9042:9042 -d -v /home/damian/go/src/healthmonitor/healthmonitordata/cassandra/data:/var/lib/cassandra cassandra 
 
 sleep 10s
 
 echo -e "Starting Elasticsearch"
 docker-compose -f docker/docker-compose-elasticsearch.yml up -d
-
-sleep 4m
-
-echo -e "Starting Healthmonitor API"
-/home/$USER/go/src/healthmonitor/healthmonitorapi/healthmonitorapi &
-
-sleep 10s
-
-echo -e "Starting Healthmonitor UI"
-cd /home/$USER/go/src/healthmonitor/healthmonitorui/ 
-npm run serve
